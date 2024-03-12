fdbb952a13db774014db6d7628547d647935522b
Nimrod León <nleonc14@gmail.com>
2024-03-12 10:29:28 -0500

    feat: generate dashboard-container component

---
+++ src/app/common/containers/dashboard-container/dashboard-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-container.component.html'
+})
+export class DashboardContainerComponent {
+
+}

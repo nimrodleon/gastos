9d841cd02e508b7600ef88f203229d6125feaa01
Nimrod León <nleonc14@gmail.com>
2024-03-12 11:22:41 -0500

    feat: generate component structure

---
+++ src/app/balance/pages/balance-report/balance-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './balance-report.component.html',
+  styleUrl: './balance-report.component.scss'
+})
+export class BalanceReportComponent {
+
+}
---
+++ src/app/income/pages/income-list/income-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-income-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './income-list.component.html',
+  styleUrl: './income-list.component.scss'
+})
+export class IncomeListComponent {
+
+}

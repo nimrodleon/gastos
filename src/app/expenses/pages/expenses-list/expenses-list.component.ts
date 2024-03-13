88e62b9074d6760ca5df9b9b1b7ee37ed9aa0f53
Nimrod León <nleonc14@gmail.com>
2024-03-12 19:54:35 -0500

    feat: html modal gasto

--- src/app/expenses/pages/expenses-list/expenses-list.component.ts
+++ src/app/expenses/pages/expenses-list/expenses-list.component.ts
@@ -8,6 +8,7 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { TableModule } from 'primeng/table';
 import { Product } from '../../interfaces';
+import { ExpenseFormModalComponent } from '../../components/expense-form-modal/expense-form-modal.component';
 
 @Component({
   selector: 'app-expenses-list',
@@ -21,6 +22,7 @@ import { Product } from '../../interfaces';
     IconFieldModule,
     InputIconModule,
     TableModule,
+    ExpenseFormModalComponent,
   ],
   templateUrl: './expenses-list.component.html',
   styleUrl: './expenses-list.component.scss'
@@ -29,5 +31,14 @@ export class ExpensesListComponent {
   rangeDates: Date[] | undefined;
   query: string = "";
   products!: Product[];
+  visibleModal: boolean = false;
+
+  public addExpense(): void {
+    this.visibleModal = true;
+  }
+
+  public changeVisibleModal(value: boolean): void {
+    this.visibleModal = value;
+  }
 
 }

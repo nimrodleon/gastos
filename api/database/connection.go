7f381d4538ba19a715c0c7f74376bd9f1497abcb
Nimrod León <nleonc14@gmail.com>
2024-03-13 16:52:21 -0500

    refactoring connection database

--- api/database/connection.go
+++ api/database/connection.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"sync"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	db   *gorm.DB
+	once sync.Once
+)
+
+func Connect() (*gorm.DB, error) {
+	var err error
+	once.Do(func() {
+		dsn := "root:sysadmin@tcp(127.0.0.1:3306)/gastos?charset=utf8mb4&parseTime=True&loc=Local"
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	})
+	return db, err
+}
+
+// func Connect() (*gorm.DB, error) {
+// 	dsn := "root:sysadmin@tcp(127.0.0.1:3306)/gastos?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return db, nil
+// }
--- api/go.mod
+++ api/go.mod
@@ -4,9 +4,13 @@ go 1.21.5
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.2
-	gorm.io/gorm v1.25.0
+	github.com/google/uuid v1.5.0
+	gorm.io/driver/mysql v1.5.4
+	gorm.io/gorm v1.25.7
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/google/uuid v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
@@ -18,4 +22,5 @@ require (
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 )

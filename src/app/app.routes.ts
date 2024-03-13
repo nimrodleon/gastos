397912d2a1c91dba7b6144816f276bdfff38ba50
Nimrod León <nleonc14@gmail.com>
2024-03-12 21:20:05 -0500

    feat: html ingresos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BalanceReportComponent } from './balance/pages/balance-report/balance-report.component';
 import { ExpensesListComponent } from './expenses/pages/expenses-list/expenses-list.component';
+import { IncomeListComponent } from './income/pages/income-list/income-list.component';
 
 export const routes: Routes = [
   { path: "", component: BalanceReportComponent },
-  { path: "expenses", component: ExpensesListComponent }
+  { path: "expenses", component: ExpensesListComponent },
+  {path: "income-list", component: IncomeListComponent}
 ];

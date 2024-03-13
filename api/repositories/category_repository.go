168cb34402dd4da889a0a7724f4d87d2161d71ad
Nimrod León <nleonc14@gmail.com>
2024-03-13 17:36:26 -0500

    add: category_repository created

---
+++ api/repositories/category_repository.go
@@ -0,0 +1,44 @@
+package repositories
+
+import (
+	"github.com/google/uuid"
+	"github.com/nimrodleon/gastos/database"
+	"github.com/nimrodleon/gastos/models"
+	"gorm.io/gorm"
+)
+
+type CategoryRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepository() *CategoryRepository {
+	db, _ := database.Connect()
+	return &CategoryRepository{db: db}
+}
+
+func (r *CategoryRepository) GetAll(categoryType string, name string) ([]models.Category, error) {
+	var categories []models.Category
+	err := r.db.Where("CategoryType = ? AND Name LIKE ?", categoryType, "%"+name+"%").Find(&categories).Error
+	return categories, err
+}
+
+func (r *CategoryRepository) GetByID(id uuid.UUID) (*models.Category, error) {
+	var category models.Category
+	err := r.db.First(&category, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (r *CategoryRepository) Create(category *models.Category) error {
+	return r.db.Create(category).Error
+}
+
+func (r *CategoryRepository) Update(category *models.Category) error {
+	return r.db.Save(category).Error
+}
+
+func (r *CategoryRepository) Delete(id uuid.UUID) error {
+	return r.db.Delete(&models.Category{}, "id = ?", id).Error
+}

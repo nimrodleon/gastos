6eeeb6d7d8930615da873a2cb4b991691361480c
Nimrod León <nleonc14@gmail.com>
2024-03-13 15:54:33 -0500

    models created

---
+++ api/models/income.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Income struct {
+	ID         uuid.UUID `gorm:"type:uuid;primary_key;"`
+	Amount     float64
+	CategoryID uuid.UUID
+	Category   Category `gorm:"foreignKey:CategoryID"`
+	Note       string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}

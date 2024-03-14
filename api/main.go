e86933e8fe80cda8fb22f2377d711b3413abbf27
Nimrod León <nleonc14@gmail.com>
2024-03-13 19:48:21 -0500

    add: created category handler

--- api/main.go
+++ api/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/nimrodleon/gastos/database"
+	"github.com/nimrodleon/gastos/handlers"
+	"github.com/nimrodleon/gastos/models"
 )
 
 func main() {
 	app := fiber.New()
+	handlers.CategoryHandler(app)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("¡Hola, mundo!")
-	})
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// AutoMigrate models
+	db.AutoMigrate(&models.Category{})
+	db.AutoMigrate(&models.Income{})
+	db.AutoMigrate(&models.Expense{})
 
 	app.Listen(":3000")
 }
